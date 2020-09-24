3aeb9e553a8a1444a9226e9cefbb34f205027a85
hayabusabusa <hayabusabusaSY@gmail.com>
2020-09-24 13:32:45 +0900

    リポジトリ詳細画面のパースエラーを修正

--- iOSEngineerCodeCheck/RepositoryDetailViewController.swift
+++ iOSEngineerCodeCheck/RepositoryDetailViewController.swift
@@ -41,7 +41,7 @@ extension RepositoryDetailViewController {
         titleLabel.text         = repository["full_name"] as? String
         languageLabel.text      = "Written in \(repository["language"] as? String ?? "")"
         starsLabel.text         = "\(repository["stargazers_count"] as? Int ?? 0) stars"
-        watchersLabel.text      = "\(repository["wachers_count"] as? Int ?? 0) watchers"
+        watchersLabel.text      = "\(repository["watchers_count"] as? Int ?? 0) watchers"
         forksLabel.text         = "\(repository["forks_count"] as? Int ?? 0) forks"
         openIssuesLabel.text    = "\(repository["open_issues_count"] as? Int ?? 0) open issues"
     }
