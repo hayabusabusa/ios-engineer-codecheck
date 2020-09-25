c3870d4df97407525f26f4cff547c0f1f66b734e
hayabusabusa <hayabusabusaSY@gmail.com>
2020-09-25 12:35:28 +0900

    Add GitHub repository model object

---
+++ iOSEngineerCodeCheck/Network/Response/GitHubRepository.swift
@@ -0,0 +1,27 @@
+//
+//  GitHubRepository.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 山田隼也 on 2020/09/25.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+struct GitHubRepository: Decodable {
+    let fullName: String
+    let stargazersCount: Int
+    let watchersCount: Int
+    let language: String
+    let forksCount: Int
+    let openIssueCount: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case fullName           = "full_name"
+        case stargazersCount    = "stargazers_count"
+        case watchersCount      = "watchers_count"
+        case language
+        case forksCount         = "forks_count"
+        case openIssueCount     = "open_issues_count"
+    }
+}
