d49346c2926acbc2da3b665096c7618d0869a925
hayabusabusa <hayabusabusaSY@gmail.com>
2020-09-28 09:45:47 +0900

    Add get readme request

---
+++ iOSEngineerCodeCheck/Network/Request/GetReadmeRequest.swift
@@ -0,0 +1,35 @@
+//
+//  GetReadmeRequest.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 山田隼也 on 2020/09/28.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import Alamofire
+
+struct GetReadmeRequest: APICustomDecodeRequest {
+    typealias Response = String?
+    
+    let ownerName: String
+    let repositoryName: String
+    
+    var endpoint: String {
+        return "https://raw.githubusercontent.com"
+    }
+    
+    var path: String {
+        return "/\(ownerName)/\(repositoryName)/master/README.md"
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    func decode(from data: Data?) throws -> String? {
+        guard let data = data else {
+            return nil
+        }
+        return String(data: data, encoding: .utf8)
+    }
+}
